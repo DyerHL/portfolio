6b1e33ed5f31b35646927f6b88eedd50c5415012
Halie Dyer <halie.l.dyer@gmail.com>
2022-04-17 20:59:11 -0500

    about and contact section

---
+++ src/components/Contact.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import email from '../assets/email.png';
+import linkedin from '../assets/linkedin.png';
+import resume from '../assets/resume.png';
+import phone from '../assets/phone.png';
+import Halie from '../assets/Halie.pdf';
+
+export default function Contact() {
+  return (
+    <div className="contact">
+      <div className="fill">Contact</div>
+      <div className="contact-cards-container">
+        <div className="card-div">
+          <card className="contact-card">
+            <div className="contact-img">
+              <img
+                className="card-logo"
+                src={email}
+                alt="email"
+                style={{ width: '100px' }}
+              />
+            </div>
+            <div className="contact-text">
+              <div>Email:</div>
+              <div>Halie.L.Dyer@gmail.com</div>
+            </div>
+          </card>
+        </div>
+        <div className="card-div">
+          <card className="contact-card">
+            <div className="contact-img">
+              <img
+                className="card-logo"
+                src={phone}
+                alt="phone"
+                style={{ width: '100px' }}
+              />
+            </div>
+            <div className="contact-text">
+              <div>Phone:</div>
+              <div>423-361-0665</div>
+            </div>
+          </card>
+        </div>
+        <div className="card-div">
+          <card className="contact-card">
+            <div className="contact-img">
+              <img
+                className="card-logo"
+                src={linkedin}
+                alt="linedin"
+                style={{ width: '100px' }}
+              />
+            </div>
+            <div className="contact-text">
+              <a
+                className="link-text"
+                rel="noreferrer"
+                target="_blank"
+                href="https://www.linkedin.com/in/hldyer/"
+              >Linkedin
+              </a>
+              <br />
+            </div>
+          </card>
+        </div>
+        <div className="card-div">
+          <card className="contact-card">
+            <div className="contact-img">
+              <img
+                className="card-logo"
+                src={resume}
+                alt="resume"
+                style={{ width: '100px' }}
+              />
+            </div>
+            <div className="contact-text">
+              <a
+                className="link-text"
+                rel="noreferrer"
+                target="_blank"
+                href={Halie}
+              >Resume
+              </a>
+              <br />
+            </div>
+          </card>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/js/src/collapse';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand navbar-light justify-content-center fixed-top bg-white">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarTogglerDemo02"
+        aria-controls="navbarTogglerDemo02"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="" id="navbarTogglerDemo02">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <a className="nav-link" to="/about">ABOUT
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" to="/agents">CONTACT
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" to="/listings">PROJECTS
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" to="/contact">TECHNOLOGIES
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
